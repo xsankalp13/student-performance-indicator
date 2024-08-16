9cefad41cdd2053cb234628a6c834699d647ee3f
xsankalp13 <sankalpbsalve@gmail.com>
2024-08-16 20:38:14 +0530

    module: complete the data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+sys.path.insert(0, '../src')
 from src.logger import logging
 
 def error_message_details(error, error_details:sys):
@@ -15,6 +16,8 @@ class CustomException(Exception):
     def __init__(self, error_message, error_details:sys):
         super().__init__(error_message)
         self.error_message=error_message_details(error_message,error_details=error_details)
+        
     
     def __str__(self):
         return self.error_message
+    
