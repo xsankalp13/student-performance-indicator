77358009af36ae095596125acd5b72663e01c607
xsankalp13 <sankalpbsalve@gmail.com>
2024-08-18 19:25:37 +0530

    feat: added the web page developement done, added python.config for aws beanstalks

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 
 import dill
-
+import pickle
 from src.exception import CustomException
 
 from sklearn.metrics import r2_score
@@ -22,31 +22,6 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-# def evaluate_models(X_train, y_train, X_test, y_test, models, param):
-#     try:
-#         report = {}
-#         for i in range(len(list(models))):
-#             model = list(models.values())[i]
-#             if model not in param:
-#                 continue
-#             para = param[list(models.keys())[i]]
-
-#             gs= GridSearchCV(model, para, cv=3)
-#             gs.fit(X_train, y_train)
-
-
-#             model.set_params(**gs.best_params_)
-#             model.fit(X_train, y_train)
-
-#             y_test_pred = model.predict(X_test)
-
-#             test_model_score = r2_score(y_test, y_test_pred)
-
-#             report[list(models.keys())[i]] = test_model_score
-#         return report
-
-#     except Exception as e:
-#         raise CustomException(e,sys)
 
 def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
@@ -67,5 +42,12 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
 
         return report
 
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
