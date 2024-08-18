16eb74161fa4be43f07bf44dd8525ba83f05db8a
xsankalp13 <sankalpbsalve@gmail.com>
2024-08-18 19:49:46 +0530

    fix: removed port

--- application.py
+++ application.py
@@ -36,4 +36,4 @@ def predict_datapoint():
         return render_template("home.html", prediction=prediction[0])
 
 if __name__==("__main__"):
-    app.run(host="0.0.0.0",port=3100)
+    app.run(host="0.0.0.0")
